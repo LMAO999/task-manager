40946c30c2491b5a30da64841211d2a98daade1a
kbatavia12 <kbatavia12@gmail.com>
2022-01-07 12:15:33 +0530

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
-import './App.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAR8qPnYMhL3qL421hIpCaPs96c1FocNzI",
+	authDomain: "task-scheduler-113e0.firebaseapp.com",
+	projectId: "task-scheduler-113e0",
+	storageBucket: "task-scheduler-113e0.appspot.com",
+	messagingSenderId: "341258515863",
+	appId: "1:341258515863:web:2602a18b1ab66ddfdd00d2",
+	measurementId: "G-72F27YGBBX"
+};
+
+initializeApp(firebaseConfig);
 
 function App() {
 	return (
